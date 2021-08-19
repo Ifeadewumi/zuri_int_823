1a33b75264b684e8a9315ef1bc8e672cc9db2324
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2021-08-19 21:28:59 +0100

    Debug changed

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2d^6zxi^u3u0up0aweey*aq@q*e0$nr91jg&dw$lr98)y_m69&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
